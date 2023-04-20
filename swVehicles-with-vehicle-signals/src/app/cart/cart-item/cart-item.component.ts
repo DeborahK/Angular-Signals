ea84d79b3a43374aede4762db7ac4632fbf3f0af
DeborahK <deborahk@insteptech.com>
2023-04-20 11:50:39 -0700

    Replace Cart Item BehaviorSubject with signal

--- swVehicles-with-vehicle-signals/src/app/cart/cart-item/cart-item.component.ts
+++ swVehicles-with-vehicle-signals/src/app/cart/cart-item/cart-item.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, signal } from '@angular/core';
 import { AsyncPipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { BehaviorSubject, map, tap } from 'rxjs';
 import { CartItem } from '../cart';
 import { CartService } from '../cart.service';
 
@@ -20,30 +19,28 @@ export class CartItemComponent {
   }
   @Input() set item(item: CartItem) {
     this._item = item;
-    this.itemChangedSubject.next(item);
+    this.cartItem.set(item);
   }
 
   // Hard-coded quantity
   // These could instead come from an inventory system
-  qtyArr = [1, 2, 3, 4, 5, 6, 7, 8];
+  qtyArr = signal([1, 2, 3, 4, 5, 6, 7, 8]);
 
-  // Item was changed action
-  private itemChangedSubject = new BehaviorSubject<CartItem>(this.item);
-  item$ = this.itemChangedSubject.asObservable();
+  // Cart item signal
+  cartItem = signal(this.item);
 
   // When the item changes, recalculate the extended price
-  exPrice$ = this.item$.pipe(
-    map(it => it.quantity * Number(it.vehicle.cost_in_credits))
-  );
+  exPrice = computed(() =>
+    this.cartItem().quantity * Number(this.cartItem().vehicle.cost_in_credits));
 
   constructor(private cartService: CartService) { }
 
   onQuantitySelected(quantity: number): void {
     // Update the quantity in the item
-    this.cartService.updateInCart(this.item, Number(quantity));
+    this.cartService.updateInCart(this.cartItem(), Number(quantity));
   }
 
   onRemove(): void {
-    this.cartService.removeFromCart(this.item);
+    this.cartService.removeFromCart(this.cartItem());
   }
 }
