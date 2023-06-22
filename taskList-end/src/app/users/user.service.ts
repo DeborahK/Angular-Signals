7e34dffa925c4c755587ecb7df72ccaa9de1d423
DeborahK <deborahk@insteptech.com>
2023-06-22 14:56:38 -0700

    Add writable signal example to no query param case

--- taskList-end/src/app/users/user.service.ts
+++ taskList-end/src/app/users/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { catchError, of } from 'rxjs';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, of, takeUntil, tap } from 'rxjs';
 
 import { User } from './user';
 
@@ -20,8 +20,23 @@ export class UserService {
   );
   
   // Expose the state as a signal
-  users = toSignal(this.users$, {initialValue: [] as User[]});
+  // NOTE: toSignal creates a readonly signal
+  // This example does not need a writable signal
+  readOnlyUsers = toSignal(this.users$, {initialValue: [] as User[]});
   selectedUserId = signal(0);
+
+  // If you did need a writable signal:
+  // Create the writeable signal
+  // Issue the http request
+  // Use tap to set the signal from the result
+  // Unsubscribe when the component is destroyed
+  // Subscribe to issue the http request
+  users = signal<User[]>([]);
+  private sub = this.http.get<User[]>(this.userUrl).pipe(
+    tap(data => this.users.set(data)),
+    takeUntilDestroyed(),
+    catchError(() => of([] as User[]))
+  ).subscribe();
 
   // Set the selected user
   setSelectedUser(id: number) {
