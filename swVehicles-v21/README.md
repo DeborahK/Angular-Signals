6861a8ca656f42aaafe6db2799bb22c4933cec5c
DeborahK <deborahk@insteptech.com>
2025-11-25 15:44:41 -0800

    Change system files to match clean v21 install

--- swVehicles-v21/README.md
+++ swVehicles-v21/README.md
@@ -1,6 +1,6 @@
 # swVehicles
 
-This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.1.0.
+This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.0.
 
 ## Development server
 
--- swVehicles-v21/src/app/app.config.ts
+++ swVehicles-v21/src/app/app.config.ts
@@ -1,17 +1,14 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app-data';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
     importProvidersFrom(
       InMemoryWebApiModule.forRoot(AppData, { delay: 1000, passThruUnknownUrl: true })
     )
