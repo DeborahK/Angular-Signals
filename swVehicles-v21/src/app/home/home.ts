22125f6adb9f75fa2579757d8f05d8ab68d3fcdc
DeborahK <deborahk@insteptech.com>
2025-11-25 16:55:38 -0800

    Work on styles

--- swVehicles-v21/src/app/home/home.ts
+++ swVehicles-v21/src/app/home/home.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  templateUrl: './home.html'
+  templateUrl: './home.html',
+  styleUrls: ['./home.css']
 })
 export class Home {
   public pageTitle = 'Welcome to Star Wars Vehicle Sales';
